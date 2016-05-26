538e52d12878de86169e92bf70007f2c79a3f761
#云淡然 <cloudcome@163.com>
2016-05-26 15:41:51 +0800

    blear.classes.switchable

---
+++ readme.md
@@ -0,0 +1,25 @@
+# blear.classes.switchable
+
+[![npm module][npm-img]][npm-url]
+[![build status][travis-img]][travis-url]
+[![coverage][coveralls-img]][coveralls-url]
+
+[travis-img]: https://img.shields.io/travis/blearjs/blear.classes.switchable/master.svg?style=flat-square
+[travis-url]: https://travis-ci.org/blearjs/blear.classes.switchable
+
+[npm-img]: https://img.shields.io/npm/v/blear.classes.switchable.svg?style=flat-square
+[npm-url]: https://www.npmjs.com/package/blear.classes.switchable
+
+[coveralls-img]: https://img.shields.io/coveralls/blearjs/blear.classes.switchable/master.svg?style=flat-square
+[coveralls-url]: https://coveralls.io/github/blearjs/blear.classes.switchable?branch=master
+
+
+## DOM 结构
+```
+<ul id="switchable">
+    <li>
+        <a href="#target"></a>
+    </li>
+</ul>
+```
+
---
+++ src/index.js
@@ -0,0 +1,107 @@
+/**
+ * 可切换的元素
+ * @author ydr.me
+ * @create 2016年05月26日11:31:01
+ */
+
+'use strict';
+
+var events =    require('blear.classes.events');
+var object =    require('blear.utils.object');
+var time =      require('blear.utils.time');
+var event =     require('blear.core.event');
+var selector =  require('blear.core.selector');
+var attribute = require('blear.core.attribute');
+
+
+var reHash = /^#/;
+var defaults = {
+    el: null,
+    activeClass: 'active',
+    activeIndex: 0,
+    triggerEvent: 'click'
+};
+var Switchable = events.extend({
+    className: 'Switchable',
+    constructor: function (options) {
+        var the = this;
+
+        Switchable.parent(the);
+        the[_options] = object.assign({}, defaults, options);
+        the[_initNode]();
+        the[_initEvent]();
+
+        time.nextTick(function () {
+            the.change(the[_options].activeIndex);
+        });
+    },
+
+
+    /**
+     * 改变索引值
+     * @param index {Number} 索引值
+     * @returns {Switchable}
+     */
+    change: function (index) {
+        var the = this;
+        var itemEl = selector.children(the[_navEl])[index];
+
+        if (!itemEl) {
+            return the;
+        }
+
+        var anchorEl = selector.children(itemEl)[0];
+        event.emit(anchorEl, 'click');
+        return the;
+    },
+
+    destroy: function () {
+        var the = this;
+
+        event.un(the[_navEl], the[_options].triggerEvent, the[_onChange]);
+        Switchable.parent.destroy(the);
+    }
+});
+var _options = Switchable.sole();
+var _initNode = Switchable.sole();
+var _initEvent = Switchable.sole();
+var _navEl = Switchable.sole();
+var _onChange = Switchable.sole();
+var _lastIndex = Switchable.sole();
+
+
+/**
+ * 初始化元素
+ */
+Switchable.method(_initNode, function () {
+    var the = this;
+
+    the[_navEl] = selector.query(the[_options].el)[0];
+});
+
+
+/**
+ * 初始化事件
+ */
+Switchable.method(_initEvent, function () {
+    var the = this;
+
+    event.on(the[_navEl], the[_options].triggerEvent, 'a', the[_onChange] = function (ev) {
+        var el = this;
+        var parentEl = selector.parent(el)[0];
+        var index = selector.index(parentEl);
+        var href = attribute.attr(el, 'href');
+
+        if (the[_lastIndex] !== index) {
+            the[_lastIndex] = index;
+
+            the.emit('change', index, selector.query(href)[0]);
+        }
+
+        if (reHash.test(href)) {
+            ev.preventDefault();
+        }
+    });
+});
+
+module.exports = Switchable;
